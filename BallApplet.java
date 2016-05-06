7cc3ec4881ffbd35c80745609c4effae342839a8
NoahJWeiss <noah.weiss12@gmail.com>
2016-05-06 13:08:38 -0700

    Added files via upload

---
+++ BallApplet.java
@@ -0,0 +1,45 @@
+import java.applet.*; 
+import java.awt.*; 
+
+public class BallApplet extends Applet implements Runnable 
+{
+int x_pos = 10; 
+int y_pos = 100; 
+int radius = 20; 
+
+public void init() { 
+   setBackground(Color.blue);
+} 
+
+public void start() { 
+   Thread th = new Thread(this);
+   th.start();
+} 
+
+public void stop() { } 
+
+public void destroy() { } 
+
+public void run () { 
+   Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+   while(true){
+      x_pos++;
+      repaint();
+      try
+      {
+      Thread.sleep(20);
+      }
+      catch(InterruptedException ex)
+      {
+      
+      }
+      Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+   }
+} 
+
+public void paint (Graphics g) { 
+   g.setColor(Color.red);
+   g.fillOval(x_pos - radius, y_pos - radius, radius * 2, radius * 2);
+} 
+
+}

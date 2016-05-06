9ab981dfa756881944377a290241fc6c5d473989
NoahJWeiss <noah.weiss12@gmail.com>
2016-05-06 13:15:07 -0700

    Update BallApplet.java

--- BallApplet.java
+++ BallApplet.java
@@ -1,71 +1,45 @@
-import java.applet.*;
-import java.awt.*;
+import java.applet.*; 
+import java.awt.*; 
 
-public class Ballbewegung1 extends Applet implements Runnable
+public class BallApplet extends Applet implements Runnable 
 {
-	// Initialisierung der Variablen
-	int x_pos = 10;		// x - Position des Balles
-	int y_pos = 100;	// y - Position des Balles
-	int radius = 20;	// Radius des Balles
+int x_pos = 10; 
+int y_pos = 100; 
+int radius = 20; 
 
-	public void init()
-	{
-		setBackground (Color.blue);
-	}
+public void init() { 
+   setBackground(Color.blue);
+} 
 
-	public void start ()
-	{
-		// Schaffen eines neuen Threads, in dem das Spiel läuft
-		Thread th = new Thread (this);
-		// Starten des Threads
-		th.start ();
-	}
+public void start() { 
+   Thread th = new Thread(this);
+   th.start();
+} 
 
-	public void stop()
-	{
+public void stop() { } 
 
-	}
+public void destroy() { } 
 
-	public void destroy()
-	{
+public void run () { 
+   Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+   while(true){
+      x_pos++;
+      repaint();
+      try
+      {
+      Thread.sleep(20);
+      }
+      catch(InterruptedException ex)
+      {
+      
+      }
+      Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+   }
+} 
 
-	}
-
-	public void run ()
-	{
-		// Erniedrigen der ThreadPriority um zeichnen zu erleichtern
-		Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
-
-		// Solange true ist läuft der Thread weiter
-		while (true)
-		{
-			// Verändern der x- Koordinate
-			x_pos ++;
-
-			// Neuzeichnen des Applets
-			repaint();
-
-			try
-			{
-				// Stoppen des Threads für in Klammern angegebene Millisekunden
-				Thread.sleep (20);
-			}
-			catch (InterruptedException ex)
-			{
-				// do nothing
-			}
-
-			// Zurücksetzen der ThreadPriority auf Maximalwert
-			Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
-		}
-	}
-
-
-	public void paint (Graphics g)
-	{
-		g.setColor  (Color.red);
-
-		g.fillOval (x_pos - radius, y_pos - radius, 2 * radius, 2 * radius);
-	}
+public void paint (Graphics g) { 
+   g.setColor(Color.red);
+   g.fillOval(x_pos - radius, y_pos - radius, radius * 2, radius * 2);
+} 
 
 }
